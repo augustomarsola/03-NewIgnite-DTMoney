2abb08a4372e747035dd8b3b2b9094ae6f1a35af
Augusto Marsola <augusto.marsola@live.com>
2022-08-16 23:03:25 -0300

    Initiate the header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./global";
+import { Transaction } from "./pages";
 import { defaultTheme } from "./themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <Transaction />
     </ThemeProvider>
   );
 }
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Header } from "../components";
+
+export function Transaction() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
