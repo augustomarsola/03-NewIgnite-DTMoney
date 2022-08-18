cb989090fcb7f2429b82c9037a90cf2c7b1d3c60
Augusto Marsola <augusto.marsola@live.com>
2022-08-18 07:02:11 -0300

    Added the initial Summary style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import logo from "../assets/logo.svg";
+import logo from "../../assets/logo.svg";
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 export function Header() {
